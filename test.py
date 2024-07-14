9d1a1ae14aea9cd9c8c6b5d50566807ec52cc635
DJChiam <dajie.chiam@gmail.com>
2024-07-15 00:22:38 +0800

    test: Update test py file

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
+"""
+Module docstring: This module is for testing GitHub workflows.
+"""
+
 def testing():
+    """
+    Function docstring: This function prints a test message.
+    """
     print("Testing github workflows")
 
 testing()
+
